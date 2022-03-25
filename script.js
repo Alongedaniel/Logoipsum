2e520d8f3148004ba1502a3afe695d350e2f28ba
Alongedaniel <alongedaniel41@gmail.com>
2022-03-25 16:46:00 +0100

    Added the side menu for mobie view

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 window.addEventListener("load", () => {
-  const preloader = document.querySelector(".loader-style")
+  const preloader = document.querySelector(".loader-style");
   preloader.classList.add("preload-finish");
-
 });
+
+let menuToggler = function () {
+  let openMenu = document.querySelector(".menu-icon");
+  let closeMenu = document.querySelector(".close");
+  let mobileMenu = document.querySelector(".mobile-nav");
+  let links = document.querySelectorAll(".mobile-nav-link");
+  let body = document.querySelector("body");
+
+  openMenu.addEventListener("click", () => {
+    mobileMenu.style.transform = "translate(0px)";
+    openMenu.style.transform = "rotate(180deg)";
+  });
+  closeMenu.addEventListener("click", () => {
+    mobileMenu.style.transform = "translate(-1000px)";
+  });
+};
